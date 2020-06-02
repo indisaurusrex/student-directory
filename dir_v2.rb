468c4a695fb4572a1045bcf011dea1037231f920
India Rex <face940@gmail.com>
2020-06-02 08:36:57 +0100

    print only names under 12 characters long

--- dir_v2.rb
+++ dir_v2.rb
@@ -21,9 +21,10 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  # print the students only if their name begins with H 
+  # print the students only if their name is shorter than 12 chars
+  puts "The following students have names under 12 characters long:"
   students.each_with_index do |student, index|
-    if student[:name].to_s[0] == "H"
+    if student[:name].length < 12
       puts "#{(index + 1)}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
