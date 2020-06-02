b021b5ffed0aa2d1e6fc3a892f21871b729efa87
India Rex <face940@gmail.com>
2020-06-02 16:30:28 +0100

    updates notes within code for clearer understanding

--- dir_v2.rb
+++ dir_v2.rb
@@ -3,6 +3,7 @@ def input_students
   puts "To finish, hit return at 'Name:'".center(75)
   #create an empty array
   students = []
+  # get the first name
   puts "Name:"
   name = gets.chomp
   while !name.empty? do
@@ -12,18 +13,21 @@ def input_students
       if cohort == ""
         cohort = "november"
       end
+
     puts "Check your answer, type yes for correct, type no to redo".center(75)
     puts "name = #{name}, cohort = #{cohort}".center(75)
     # only commit the information to the array if user is happy
     check = gets.chomp
     if check == "yes"
       students << {name: name, cohort: cohort.to_sym}
+      # confirm how many students are in the array now
       if students.length == 1
         puts "Now we have 1 student".center(75)
       else
         puts "Now we have #{students.count} students".center(75)
       end
     end
+    # get more students
     puts "Name:"
     name = gets.chomp
   end
@@ -34,12 +38,13 @@ def print_header
   puts "The students of Villains Academy".center(75)
   puts "-------------".center(75)
 end
-# print the students and their cohort, height and hobby, centred
+# print the students and their cohort, centred
 def print(students)
   students.each_with_index do |student, index|
     puts "#{(index + 1)}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
   end
 end
+# print how many students we have overall
 def print_footer(students)
   if students.count == 1
     puts "Overall, we have 1 great student".center(75)
