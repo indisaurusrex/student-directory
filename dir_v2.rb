ec8c78123198d6ccd414479649237dce69236b88
India Rex <face940@gmail.com>
2020-06-03 16:24:30 +0100

    adds functionality to save student list to csv

--- dir_v2.rb
+++ dir_v2.rb
@@ -53,6 +53,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # will cause program to terminate
   else
@@ -71,6 +73,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 bc there'll be more later
 end
 
@@ -85,6 +88,18 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
