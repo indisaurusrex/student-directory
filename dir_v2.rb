3df2d07721aa4f50833b96aed72e559eeccf4c06
India Rex <face940@gmail.com>
2020-06-03 10:02:41 +0100

    uses .chop, .gsub and .strip instead of .chomp after gets commands for user input

--- dir_v2.rb
+++ dir_v2.rb
@@ -5,21 +5,25 @@ def input_students
   students = []
   # get the first name
   puts "Name:"
-  name = gets.chomp
+  name = gets.gsub("\n", "")
+
   while !name.empty? do
     puts "Cohort:"
-    cohort = gets.chomp.downcase
+    cohort = gets.strip.downcase
     # if the user types nothing for cohort, default to Nov
       if cohort == ""
         cohort = "november"
       end
     # only commit the information to the array if user is happy
-    puts "Check your answer, type yes for correct, type no to redo".center(75)
+
+    puts "Check your answer, type yes if correct, type no to redo".center(75)
     puts "name = #{name}, cohort = #{cohort}".center(75)
-    check = gets.chomp
+
+    check = gets.chop
     if check == "yes"
       students << {name: name, cohort: cohort.to_sym}
       # confirm how many students are in the array now
+
       if students.length == 1
         puts "Now we have 1 student".center(75)
       else
@@ -28,11 +32,12 @@ def input_students
     end
     # get more students
     puts "Name:"
-    name = gets.chomp
+    name = gets.gsub("\n", "")
   end
   #return the array of students
   students
 end
+
 def print_header
   puts "The students of Villains Academy".center(75)
   puts "-------------".center(75)
@@ -40,7 +45,7 @@ end
 
 def print_students(students)
     students.each do |student|
-      puts "#{student[:name]} : (#{student[:cohort]}) cohort".center(75)
+      puts "#{student[:name]} : #{student[:cohort]} cohort".center(75)
     end
 end
 
@@ -57,4 +62,3 @@ students = input_students
 print_header
 print_students(students)
 print_footer(students)
-# sort_into_cohorts
