887e2d0ad8d8dcc05434520ba9d497777d574dfe
India Rex <face940@gmail.com>
2020-06-03 15:50:36 +0100

    adds interactive menu

--- dir_v2.rb
+++ dir_v2.rb
@@ -58,7 +58,31 @@ def print_footer(students)
   end
 end
 
-students = input_students
-print_header
-print_students(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    # print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more options later
+    # read the input and save it as a variable
+    selection = gets.chomp
+    # do what the user has asked
+    case selection
+    when "1" 
+      # input the students
+      students = input_students
+    when "2"
+      # show the students
+      print_header
+      print_students(students)
+      print_footer(students)
+    when "9"
+      exit # this will terminate the program
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
+
+interactive_menu
